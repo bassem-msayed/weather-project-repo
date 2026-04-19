706c43cbd3d179ff0da3f2e48a966d48044f6eb0
Bassem <Bassem.m.sayed@outlook.com>
2026-04-19 19:29:10 +0200

    intermediate: some columns were missing source table forr the join

--- models/intermediate/int_climate_joined.sql
+++ models/intermediate/int_climate_joined.sql
@@ -10,11 +10,11 @@ joined as (
         d.date,
         extract(year from d.date) as year_num,
         extract(month from d.date) as month_num,
-        percipitation_mm,
-        solar_radiation_mj,
-        evapotranspiration_mm,
-        temp_min,
-        temp_max
+        d.percipitation_mm,
+        d.solar_radiation_mj,
+        d.evapotranspiration_mm,
+        d.temp_min,
+        d.temp_max
     from daily d
     left join cities c
         on d.locationid = c.locationid
