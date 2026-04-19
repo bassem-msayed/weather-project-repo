142c9b825d8f565f6bc897ad666d7162c264d2ac
Bassem <Bassem.m.sayed@outlook.com>
2026-04-19 13:33:12 +0200

    staging: updated indentation for better visibility

--- models/staging/stg_cities.sql
+++ models/staging/stg_cities.sql
@@ -4,15 +4,15 @@ with source as(
 ),
 renamed as(
     select
-        cast(location_id as integer)    as location_id,
-        cast(latitude as float64)       as latitude,
-        cast(longitude as float64)      as longitude,
-        cast(elevation as float64)      as elevation,
+        cast(location_id as integer) as location_id,
+        cast(latitude as float64) as latitude,
+        cast(longitude as float64) as longitude,
+        cast(elevation as float64) as elevation,
         case 
-            when (cast(latitude as float64) = 55.75 and cast(longitude as float) = -4.25)   then 'Glasgow'
-            when (cast(latitude as float64) = 25 and cast(longitude as float) = 55.25)      then 'Dubai'
-            when (cast(latitude as float64) = 30 and cast(longitude as float) = 31.25)      then 'Cairo'
-            end                         as city_name
+            when (cast(latitude as float64) = 55.75 and cast(longitude as float) = -4.25) then 'Glasgow'
+            when (cast(latitude as float64) = 25 and cast(longitude as float) = 55.25) then 'Dubai'
+            else 'Cairo'
+            end as city_name
     from source
 )
 select * from renamed
