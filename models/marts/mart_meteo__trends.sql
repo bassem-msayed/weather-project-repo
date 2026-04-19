6db52fb2f493614da7017a861d26f9cbe1c48a3c
Bassem <Bassem.m.sayed@outlook.com>
2026-04-19 19:48:15 +0200

    marts: added skeleton mart model

---
+++ models/marts/mart_meteo__trends.sql
@@ -0,0 +1,18 @@
+with base as (
+    select * from {{ ref('int_meteo__joined') }}
+),
+annual as (
+    select
+        city_name,
+        year_num,
+        avg(temp_max) as avg_temp_max,
+        avg(temp_min) as avg_temp_min,
+        avg(solar_radiation_mj) as avg_solar_radiation_mj,
+        avg(precipitation_mm) as avg_precipcation_mm,
+        avg(evapotranspiration_mm) as avg_evapotranspiration_mm,
+        avg(precipitation_mm) - avg(evapotranspiration_mm) as water_deficit_mm,
+    from base
+)
+select * 
+from annual
+group by city_name, year_num
