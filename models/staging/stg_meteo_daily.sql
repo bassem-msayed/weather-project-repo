2e62eb95b189596b08cac8685a54636744553ed0
Bassem <Bassem.m.sayed@outlook.com>
2026-04-19 19:08:45 +0200

    intermediate: one of the staging columns had a typo done when fixing for the text values

--- models/staging/stg_meteo_daily.sql
+++ models/staging/stg_meteo_daily.sql
@@ -4,13 +4,13 @@ with source as(
 ),
 renamed as(
     select 
-        cast(location_id                               as integer)  as location_id,
-        cast(time                                      as date)     as date,
-        cast(temperature_2m_max______C_                as float64)  as temp_max,
-        cast(temperature_2m_min______C_                as float64)  as temp_min,
-        cast(nullif(et0_fao_evapotranspiration, 'NaN') as float64)  as evapotranspiration_mm,
-        cast(nullif(shortwave_radiation_sum,    'NaN') as float64)  as solar_radiation_mj,
-        cast(nullif(precipitation_sum,          'NaN') as float64)  as precipitation_mm
+        cast(location_id                                           as integer)  as location_id,
+        cast(time                                                  as date)     as date,
+        cast(temperature_2m_max______C_                            as float64)  as temp_max,
+        cast(temperature_2m_min______C_                            as float64)  as temp_min,
+        cast(nullif(et0_fao_evapotranspiration__mm_,        'NaN') as float64)  as evapotranspiration_mm,
+        cast(nullif(shortwave_radiation_sum__MJ_m_____,     'NaN') as float64)  as solar_radiation_mj,
+        cast(nullif(precipitation_sum__mm_,                 'NaN') as float64)  as precipitation_mm
     from source
     where time is not null
 )
